1dcfc77f2e5d2621ab3e33b229d11d6cbf10b2e9
Pote Prathumma <prathummapote@gmail.com>
2025-08-20 15:45:26 +0700

    build: Working on add buttons and interactivity

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
+const productName = document.getElementById("product-name");
 const productDescription = document.getElementById("product-description");
 const productImage = document.querySelector("#product-image");
 const addVariantBtn = document.querySelector("#add-variant-btn");
 const addProductBtn = document.querySelector("#add-product-btn");
-
-addProductBtn.addEventListener("click", () => {
-  const productName = document.getElementById("product-name");
-  alert(productName.value);
-});
+const variantName1 = document.getElementById("variant1-name");
+const variantPrice1 = document.getElementById("variant1-price");
 
 let productList = [];
+let currentVariants = [];
 
-let productObj = {
-  name: null,
-  description: null,
-  img: null,
-  variants: [],
-  id: null,
-};
+addVariantBtn.addEventListener("click", () => {
+  const newVariant = {
+    id: 1,
+    title: variantName1.value,
+    price: variantPrice1.value,
+  };
+
+  currentVariants.push(newVariant);
+  console.log(currentVariants);
+});
+
+addProductBtn.addEventListener("click", () => {
+  const newProductObj = {
+    id: `${Date.now()}-${Math.floor(Math.random() * 1000)}`, // Unique time-base id
+    name: productName.value,
+    description: productDescription.value,
+    img: productImage.value,
+    variant: currentVariants,
+  };
+
+  currentVariants = [];
+
+  console.log(newProductObj);
+  console.log(currentVariants);
+});
